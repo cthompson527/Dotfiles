67fa7d17ae36682ce4a1bf15898a1b02d985ab52
Cory Thompson <cory.thompson527@gmail.com>
2021-10-25 04:55:29 +0000

    Install conan and some extra touches to the arch script

--- arch_setup.sh
+++ arch_setup.sh
@@ -2,38 +2,38 @@
 
 set -euxo pipefail
 
-echo initial setup of mirrors...
+#### initial setup of mirrors... ####
 curl -Lo ~/mirrorlist 'https://archlinux.org/mirrorlist/?country=US&protocol=http&protocol=https&ip_version=4'
 sed -i 's/#S/S/g' ~/mirrorlist
 sudo mv -v ~/mirrorlist /etc/pacman.d/mirrorlist
 sudo pacman -S --noconfirm pacman-contrib
 
-echo determine fastest mirrors...
+#### determine fastest mirrors... ####
 rankmirrors -n 10 --max-time 1 /etc/pacman.d/mirrorlist > ~/mirrorlist.fastest
 sudo mv -v ~/mirrorlist.fastest /etc/pacman.d/mirrorlist
 
-echo update system...
-sudo pacman -Syyu
+#### update system... ####
+sudo pacman -Syu --noconfirm
 
-echo installing dependencies...
+#### installing dependencies... ####
 sudo pacman -S --noconfirm base-devel cmake unzip ninja tree-sitter curl git nodejs ncurses libffi glib2 lazygit
 sudo pacman -S --noconfirm community/tig community/ripgrep community/yarn
 sudo pacman -S --noconfirm extra/llvm extra/clang extra/python-pip
 
-echo retrieving dotfiles...
+#### retrieving dotfiles... ####
 git clone https://gitlab.com/cthompson527/dotfiles.git ~/dotfiles
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 
-echo installing neovim...
+#### installing neovim... ####
 git clone https://github.com/neovim/neovim
 pushd neovim
 make -j4
 sudo make install
 popd
 
-echo installing pathogen...
+#### installing pathogen... ####
 mkdir -p ~/.config/nvim/autoload ~/.config/nvim/bundle
 curl -LSso ~/.config/nvim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 mkdir ~/.vim
@@ -41,8 +41,9 @@ ln -s ~/.config/nvim/autoload ~/.vim/autoload
 ln -s ~/.config/nvim/bundle ~/.vim/bundle
 echo 'source ~/.vimrc' >  ~/.config/nvim/init.vim
 
-echo installing plugins...
+#### installing plugins... ####
 pushd ~/.config/nvim/bundle
+git clone https://github.com/joshdick/onedark.vim.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/preservim/nerdtree.git
 git clone https://github.com/nvim-lua/plenary.nvim.git
@@ -54,9 +55,10 @@ git clone https://github.com/neoclide/coc.nvim.git
 popd
 nvim +'CocInstall -sync coc-clangd' +qall
 
-echo setup git...
+#### setup git... ####
 git config --global user.email "cory.thompson527@gmail.com"
 git config --global user.name "Cory Thompson"
 
-echo install conan...
+#### install conan... ####
+pip install conan
 
