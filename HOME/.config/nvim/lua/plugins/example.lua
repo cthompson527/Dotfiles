c3d7d47e0c366dc789638f8c479d8de12eba7bee
Cory Thompson <cory.thompson527@gmail.com>
2025-10-22 13:59:30 -0500

    Clean up after upgrading

--- HOME/.config/nvim/lua/plugins/example.lua
+++ HOME/.config/nvim/lua/plugins/example.lua
@@ -28,7 +28,7 @@ return {
       -- Only one of these is needed.
       "nvim-telescope/telescope.nvim", -- optional
       "ibhagwan/fzf-lua", -- optional
-      "echasnovski/mini.pick", -- optional
+      "nvim-mini/mini.pick", --optional
     },
     config = true,
   },
@@ -166,10 +166,6 @@ return {
     },
   },
 
-  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
@@ -221,15 +217,9 @@ return {
     end,
   },
 
-  -- use mini.starter instead of alpha
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
-
-  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
-
   -- add any tools you want to have installed below
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "stylua",
