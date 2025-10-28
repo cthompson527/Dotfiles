01be6d5914f4786c580ce0c28fd46dd1e0a9b141
Cory Thompson <cory.thompson@mantl.com>
2025-10-28 09:06:09 -0500

    Clean up after setup for work computer

--- HOME/.config/fish/config.fish
+++ HOME/.config/fish/config.fish
@@ -16,6 +16,25 @@ function fish_user_key_bindings
     bind ! bind_bang
 end
 
-alias lg="lazygit"
+set -xg PAGER "bat --wrap=never"
+set -xg CONFIG_DIR "$HOME/.config/fish"
 
-source ~/.bash_aliases
+if [ -f "$CONFIG_DIR/work_variables.fish" ]
+    . "$CONFIG_DIR/work_variables.fish"
+end
+
+if [ -f "$CONFIG_DIR/aliases.fish" ]
+    . "$CONFIG_DIR/aliases.fish"
+end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/corythompson/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/corythompson/google-cloud-sdk/path.fish.inc'
+end
+
+# pnpm
+set -gx PNPM_HOME /Users/corythompson/Library/pnpm
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
